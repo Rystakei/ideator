1914731902a26d4b05498c633f3dad0f23254af0
MakerSquare Student <student@lab-3.att.net>
2013-06-21 14:47:38 -0500

    Deleted text and string from ideas table

--- db/migrate/20130618205237_create_ideas.rb
+++ db/migrate/20130618205237_create_ideas.rb
@@ -3,10 +3,7 @@ class CreateIdeas < ActiveRecord::Migration
     create_table :ideas do |t|
       t.string :name
       t.string :description
-      t.string :text
       t.string :picture
-      t.string :string
-
       t.timestamps
     end
   end
